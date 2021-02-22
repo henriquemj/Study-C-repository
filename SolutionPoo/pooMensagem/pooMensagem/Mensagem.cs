35114763507623301ffcb11754f547b9056bfd32
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-02-22 18:56:12 -0300

    Estudo de encapsulamento com csharp - POO

---
+++ SolutionPoo/pooMensagem/pooMensagem/Mensagem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pooMensagem
+{
+    public class Mensagem
+    {
+        private string TextoMensagem;
+
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
+        }
+        //Get serve para pegar o valor da propriedade
+        public String getTextoMensagem()
+        {
+            return this.TextoMensagem;
+        }
+        //Set definir um valor para uma propriedade que esta protegida, que esta(...)
+        //(...)encapsulada usando o private
+        public void setTextoMensagem(string valor)
+        {
+            // "ToUpper" Retorna uma cópia desta string convertida em maiúsculas.
+            this.TextoMensagem = valor.ToUpper();
+        }
+    }
+}
---
+++ SolutionPoo/pooMensagem/pooMensagem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pooMensagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Mensagem msg1;
+            msg1 = new Mensagem();
+            msg1.setTextoMensagem("Alô Mundo");
+            Console.WriteLine(msg1.getTextoMensagem());
+
+            Console.ReadKey();
+
+        }
+    }
+}
