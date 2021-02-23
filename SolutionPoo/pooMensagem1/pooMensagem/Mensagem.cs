b7c16612cbadbca1708434f9247f072e3e78f420
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-02-22 23:07:08 -0300

    Exercicio cálculadora da idade

---
+++ SolutionPoo/pooMensagem1/pooMensagem/Mensagem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pooMensagem
+{
+    public class Mensagem
+    {
+        private string textoMensagem;
+
+        public String TextoMensagem
+        {
+            get
+             {
+                return this.textoMensagem;
+             }
+
+            set
+            {
+                this.textoMensagem = value.ToUpper();
+            }
+        }
+
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem);
+        }
+    }
+}
---
+++ SolutionPoo/pooMensagem1/pooMensagem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pooMensagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Mensagem msg1;
+            msg1 = new Mensagem();
+            msg1.TextoMensagem = "Alô Mundo";
+            msg1.ExibirMensagem();
+
+            Console.ReadKey();
+
+        }
+    }
+}
