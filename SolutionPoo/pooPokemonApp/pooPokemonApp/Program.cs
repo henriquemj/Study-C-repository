47988a6f3b2ccf80b3ef8a1cce968036c37262cb
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-02-24 21:15:48 -0300

    POO - App Pokemon

---
+++ SolutionPoo/pooPokemonApp/pooPokemonApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon p = new Pokemon("#001 Bulbasaur", "Bulbasaur é um pokémon do tipo Planta Venenoso");
+            p.ExibirDadosPokemon();
+            Console.ReadKey();
+        }
+    }
+}
