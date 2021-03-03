8d2c136ea03132b6943205551463b371d96ddddf
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-03-02 23:38:40 -0300

    Exercicios com POO Zoologico

---
+++ SolutionPoo/pooZoologico/pooZoologico/AnimalPlus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooZoologico
+{
+    public class AnimalPlus: Animal
+    {
+        public AnimalPlus():base()
+            {
+            this.TipoAlimentacao = "" ;
+            }
+        public AnimalPlus(String especie, Double peso, String tipoAlimentacao) : base(especie, peso)
+        {
+            this.TipoAlimentacao = tipoAlimentacao;
+        }
+        public string TipoAlimentacao { get; set; }
+        public void ExibirDados()
+        {
+            Console.WriteLine("Especie: " + this.Especie);
+            Console.WriteLine("Peso: " + this.Peso);
+            Console.WriteLine("Tipo Alimentação: " + this.TipoAlimentacao); 
+        }
+    }
+}
---
+++ SolutionPoo/pooZoologico/pooZoologico/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooZoologico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<AnimalPlus> animais = new List<AnimalPlus>();
+            int countCar = 0, countHer = 0;
+            Console.WriteLine("Programa que conta quantos animais são carnívoros e herbívoros");
+            for (int i = 0; i < 4; i++)
+            {
+                AnimalPlus animal = new AnimalPlus();
+                Console.Write("Especie: ");
+                animal.Especie = Console.ReadLine();
+                Console.Write("Peso");
+                animal.Peso = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Tipo de alimentação: ");
+                animal.TipoAlimentacao = Console.ReadLine();
+                if(animal.TipoAlimentacao == "Carnivoro")
+                {
+                    // carnivoro
+                    countCar++;
+                }
+                else
+                {
+                    countHer++;
+                }
+                animais.Add(animal);
+            }
+            Console.WriteLine("Total de animais carnivoros: " + countCar);
+            Console.WriteLine("Total de animais herbivoros: " + countHer);
+            Console.ReadKey();
+        }
+    }
+}
